3af2249f6cd4ab1998c91b86c7614a3b92ff583e
kodyyuen <kody.yuen@gmail.com>
2023-08-31 13:28:18 -0400

    updated origin whitelist

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://dev--celebrated-begonia-8703fb.netlify.app/",
+    ],
   })
 );
 app.use(
