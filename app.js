cf693b14c9fd951abccd3870e939c1635caccbbb
kodyyuen <kody.yuen@gmail.com>
2023-08-31 15:12:10 -0400

    changed cookie session settings

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
-import session from "cookie-session";
+// import session from "cookie-session";
 import mongoose from "mongoose";
 import UsersController from "./users/users-controller.js";
 import SongsController from "./songs/songs-controller.js";
 import SpotifyController from "./spotify/spotify-controller.js";
 import "dotenv/config";
+import { MemoryStore, session } from "express-session";
 
 mongoose.connect(`${process.env.DB_CONNECTION_STRING}`);
 
@@ -25,9 +26,10 @@ app.use(
     secret: "should be an environment variable",
     resave: false,
     saveUninitialized: true,
-    secure: false,
-    maxAge: 24 * 60 * 60 * 1000,
-    // cookie: { secure: false },
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000,
+    }),
   })
 );
 app.use(express.json());
